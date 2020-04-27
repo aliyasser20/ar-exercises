84be8ff048d2e2d6558f72070f9e54a4416d0574
aliyasser20 <aliyasser20@hotmail.com>
2020-04-27 18:25:35 -0400

    exercise 1

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -1,6 +1,15 @@
+# frozen_string_literal: true
+
 require_relative '../setup'
 
-puts "Exercise 1"
-puts "----------"
+puts 'Exercise 1'
+puts '----------'
 
 # Your code goes below here ...
+store_one = Store.create(name: 'Burnaby', annual_revenue: 300_000, mens_apparel: true, womens_apparel: true)
+
+store_two = Store.create(name: 'Richmond', annual_revenue: 300_000, mens_apparel: true, womens_apparel: true)
+
+store_three = Store.create(name: 'Gastown', annual_revenue: 300_000, mens_apparel: true, womens_apparel: true)
+
+puts '---', Store.count
